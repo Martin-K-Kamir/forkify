ef81ac71a3fc2e0b66dfdc3521f7d650e7c8769e
maritn-k-kamir <martin.k.kamir@gmail.com>
2021-09-08 01:52:03 +0200

    Init commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'da9c3eed-f355-4faa-b137-d8ba9f1eb66d';
+export const MODAL_CLOSE_SEC = 2.5;
