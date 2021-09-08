f109a777d37bdeb34ac71e4a922dfabde50be6f8
maritn-k-kamir <martin.k.kamir@gmail.com>
2021-09-08 19:29:52 +0200

    Changed DEV API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'da9c3eed-f355-4faa-b137-d8ba9f1eb66d';
+export const KEY = '888e4e8a-1045-413e-9c86-4cc67ba22a98';
 export const MODAL_CLOSE_SEC = 2.5;
