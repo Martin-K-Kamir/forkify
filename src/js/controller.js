5e21f9c7ff3588abd7f4c757659f7e57b411b821
maritn-k-kamir <martin.k.kamir@gmail.com>
2021-09-08 02:00:27 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -118,6 +118,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeatures = function () {
+  console.log('Welcome to the app');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -126,6 +130,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('hi');
+  newFeatures();
 };
 init();
