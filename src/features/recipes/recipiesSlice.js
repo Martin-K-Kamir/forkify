23ea37f69574e9bfb3f30122f93c5f694e19e247
Martin Kamír <martin.k.kamir@gmail.com>
2023-10-19 18:16:02 +0200

    wip RecipesList

--- src/features/recipes/recipiesSlice.js
+++ src/features/recipes/recipiesSlice.js
@@ -1,6 +1,8 @@
 import { api } from "../api/api";
-import { createEntityAdapter } from "@reduxjs/toolkit";
+import { createEntityAdapter, createSelector } from "@reduxjs/toolkit";
 import { dataSearchQueries } from "../../dataSearchQueries.js";
+import { sub } from "date-fns";
+
 
 export const recipesAdapter = createEntityAdapter({
     sortComparer: (a, b) => b.date.localeCompare(a.date),
@@ -20,6 +22,22 @@ const extendedApi = api.injectEndpoints({
                     );
                 },
             }),
+            transformResponse: result => {
+                let min = 1;
+                let hour = 1;
+                let day = 1;
+
+                const recipes = result.data.recipes.map(post => {
+                    if (!post?.date)
+                        post.date = sub(new Date(), {
+                            minutes: min++,
+                            hours: hour++,
+                            days: day++,
+                        }).toISOString();
+                    return post;
+                });
+                return recipesAdapter.setAll(initialState, recipes);
+            },
             transformErrorResponse(result, meta, arg) {
                 if (result.data.data.recipes.length === 0) {
                     return { message: `No recipes found for search "${arg}"` };
@@ -47,3 +65,17 @@ export const {
     useLazyGetRecipesQuery,
     useGetSearchRecipesQuery,
 } = extendedApi;
+
+export const selectRecipesResult = extendedApi.endpoints.getRecipes.select();
+
+const selectRecipesData = createSelector(
+    selectRecipesResult,
+    recipesResult => recipesResult.data
+);
+
+export const {
+    selectAll: selectAllRecipes,
+    selectById: selectRecipeById,
+    selectIds: selectRecipeIds,
+    selectTotal: selectTotalRecipes,
+} = recipesAdapter.getSelectors(state => selectRecipesData(state) ?? initialState);
