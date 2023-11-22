4bef8408bfabc733734bb0b590e5524057703c77
Martin Kamír <martin.k.kamir@gmail.com>
2023-11-22 23:05:58 +0100

    refactored Select

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,35 +1,49 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import classNames from "classnames";
 import Icon from "./Icon.jsx";
 import { nanoid } from "@reduxjs/toolkit";
 
-const Select = ({label, options, value, onChange, size = "md", className, backgroundClassName, ...rest}) => {
+const Select = ({
+    label,
+    options,
+    value,
+    onChange,
+    className,
+    clearDefaultClassNames = false,
+    size = "md",
+    textActiveClassName = "text-blue-200",
+    backgroundClassName = "bg-zinc-800",
+    backgroundHoverClassName = "bg-zinc-700//hover",
+    backgroundFocusClassName = "bg-zinc-700//focus",
+    ...rest
+}) => {
+    const ref = useRef();
     const idRef = useRef(nanoid());
+
     const [currentValue, setCurrentValue] = useState(value);
     const [isOpen, setIsOpen] = useState(false);
-    const ref = useRef();
 
     const selectClasses = classNames(
         "relative no-select",
         {
             "radius-1": !isOpen,
             "radius-top-1 shadow-xl": isOpen,
-            "bg-zinc-800": !backgroundClassName,
-            "f-size--1": size === "sm" || size === "md",
-            "f-size-1": size === "lg",
+            "f-size--1":
+                size === "sm" || (size === "md" && !clearDefaultClassNames),
+            "f-size-1": size === "lg" && !clearDefaultClassNames,
+            [backgroundClassName]: !clearDefaultClassNames,
         },
-        className,
-        backgroundClassName
+        className
     );
 
     const labelClasses = classNames(
-        "cursor-pointer flex align-items-center justify-content-between gap-xs line-height-1 text-nowrap",
+        "cursor-pointer flex align-items-center justify-content-between line-height-1 text-nowrap",
         {
-            "px-2xs py-3xs": size === "sm",
-            "px-xs py-2xs": size === "md",
-            "px-s py-xs": size === "lg",
+            "px-2xs py-3xs gap-3xs": size === "sm" && !clearDefaultClassNames,
+            "px-xs py-2xs gap-2xs": size === "md" && !clearDefaultClassNames,
+            "px-s py-xs gap-xs": size === "lg" && !clearDefaultClassNames,
         }
-    )
+    );
 
     const listClasses = classNames(
         "bottom-1 left-0 translate-y-full w-full absolute bg-inherit overflow-hidden",
@@ -47,60 +61,127 @@ const Select = ({label, options, value, onChange, size = "md", className, backgr
         };
     }, []);
 
-    function handleOutsideClick(e) {
-        if (!ref.current?.contains(e.target)) {
-            setIsOpen(false);
-        }
-    }
+    const getNavigationElements = () => {
+        const focusableElements =
+            'li[role="option"]:not([aria-disabled="true"])';
+        const elements = Array.from(
+            document.querySelectorAll(focusableElements)
+        );
+        const currentIndex = elements.indexOf(document.activeElement);
+        return { elements, currentIndex };
+    };
 
-    function handleClick() {
+    const toggleIsOpen = useCallback(() => {
         setIsOpen(prevIsOpen => !prevIsOpen);
-    }
+    }, []);
 
-    function handleKeyDown(e) {
-        const focusableElements = 'li[role="option"]:not([aria-disabled="true"])';
-        const elements = document.querySelectorAll(focusableElements);
-        const currentIndex = Array.from(elements).indexOf(document.activeElement);
+    const handleClick = useCallback(() => {
+        toggleIsOpen();
+    }, []);
 
-        if (e.key === "Enter") {
-            setIsOpen(prevIsOpen => !prevIsOpen);
-        } else if (e.key === "ArrowDown") {
+    const handleOutsideClick = useCallback(
+        e => {
+            if (!ref.current?.contains(e.target)) {
+                setIsOpen(false);
+            }
+        },
+        [ref]
+    );
+
+    const handleEnterKey = useCallback(e => {
+        if (e.target !== ref.current) return;
+
+        toggleIsOpen();
+    }, []);
+
+    const handleArrowDownKey = useCallback(
+        e => {
             e.preventDefault();
+            const { elements, currentIndex } = getNavigationElements();
+
             if (!isOpen) {
                 setIsOpen(true);
             } else if (currentIndex < elements.length - 1) {
                 elements[currentIndex + 1].focus();
             }
-        } else if (e.key === "ArrowUp") {
+        },
+        [isOpen]
+    );
+
+    const handleArrowUpKey = useCallback(
+        e => {
             e.preventDefault();
+            const { elements, currentIndex } = getNavigationElements();
+
             if (currentIndex > 0) {
                 elements[currentIndex - 1].focus();
             } else if (isOpen) {
                 setIsOpen(false);
                 ref.current.focus();
             }
-        } else if (e.key === "Tab" && currentIndex === elements.length - 1) {
-            setIsOpen(false);
-        }
-    }
+        },
+        [isOpen, ref]
+    );
 
-    function handleChange(option) {
-        if (option.value === currentValue?.value) {
-            onChange({value: "default"});
-            setCurrentValue({value: "default"});
-        } else {
-            onChange(option);
-            setCurrentValue(option);
-        }
+    const handleTabKey = useCallback(() => {
+        const { elements, currentIndex } = getNavigationElements();
+
+        if (currentIndex !== elements.length - 1) return;
+
         setIsOpen(false);
-    }
+    }, []);
+
+    const handleEscapeKey = useCallback(() => {
+        setIsOpen(false);
+    }, []);
+
+    const handleKeyDown = useCallback(
+        e => {
+            switch (e.key) {
+                case "Enter":
+                    handleEnterKey(e);
+                    break;
+                case "ArrowDown":
+                    handleArrowDownKey(e);
+                    break;
+                case "ArrowUp":
+                    handleArrowUpKey(e);
+                    break;
+                case "Tab":
+                    handleTabKey();
+                    break;
+                case "Escape":
+                    handleEscapeKey();
+                default:
+                    break;
+            }
+        },
+        [handleEnterKey, handleArrowDownKey, handleArrowUpKey, handleTabKey]
+    );
+
+    const handleChange = useCallback(
+        option => {
+            if (option.value === currentValue?.value) {
+                onChange({ value: "default" });
+                setCurrentValue({ value: "default" });
+            } else {
+                onChange(option);
+                setCurrentValue(option);
+            }
+            setIsOpen(false);
+        },
+        [currentValue, onChange]
+    );
 
     const renderedOptions = options.map((option, i, arr) => {
-        const classes = classNames("cursor-pointer outline-none bg-zinc-700//focus", {
-            "text-blue-200": option.value === value?.value,
-            "px-2xs py-3xs": size === "sm",
-            "px-xs py-3xs": size === "md",
-            "px-s py-2xs": size === "lg",
+        const classes = classNames("cursor-pointer outline-none", {
+            "px-2xs py-3xs": size === "sm" && !clearDefaultClassNames,
+            "px-xs py-3xs": size === "md" && !clearDefaultClassNames,
+            "px-s py-2xs": size === "lg" && !clearDefaultClassNames,
+            [textActiveClassName]:
+                option.value === value?.value && !clearDefaultClassNames,
+            [backgroundFocusClassName]: !clearDefaultClassNames,
+            [backgroundHoverClassName]: !clearDefaultClassNames,
         });
 
         return (
@@ -116,6 +197,8 @@ const Select = ({label, options, value, onChange, size = "md", className, backgr
                 aria-setsize={arr.length}
                 aria-posinset={i + 1}
                 aria-controls={`select-${idRef.current}`}
+                aria-selected={option.value === value?.value}
+                aria-labelledby={`label-${idRef.current}`}
             >
                 {option.label}
             </li>
@@ -134,26 +217,36 @@ const Select = ({label, options, value, onChange, size = "md", className, backgr
             aria-owns={`listbox-${idRef.current}`}
             aria-labelledby={`label-${idRef.current}`}
             tabIndex={0}
-            onClick={handleClick}
             onKeyDown={handleKeyDown}
         >
             <label id={`label-${idRef.current}`} className="sr-only">
                 {label}
             </label>
-            <div
-                className={labelClasses}
-            >
+            <p className="sr-only">
+                To open the select menu, press the down arrow key. To close the
+                select menu, press the escape key. Press the enter key to select
+                the currently focused option.
+            </p>
+            <div className={labelClasses} onClick={handleClick}>
                 <div>
                     {value?.label ?? label ?? "Select an option"}
                     {/* This div is used to pre-render all possible select options offscreen.
                     This ensures that the width of the select component is set to the width of the widest option,
                     preventing any width changes when different options are selected.*/}
-                    <div aria-hidden="true" className="opacity-0 invisible pointer-events-none h-0">
+                    <div
+                        aria-hidden="true"
+                        className="opacity-0 invisible pointer-events-none h-0"
+                    >
                         <div>{value?.label ?? label ?? "Select an option"}</div>
-                        {options.map(option => <div>{option.label}</div>)}
+                        {options.map(option => (
+                            <div key={option.label}>{option.label}</div>
+                        ))}
                     </div>
                 </div>
-                <Icon type={isOpen ? "expandLess" : "expandMore"} className="f-size-2"/>
+                <Icon
+                    type={isOpen ? "expandLess" : "expandMore"}
+                    className="f-size-2"
+                />
             </div>
             {isOpen && (
                 <ul
