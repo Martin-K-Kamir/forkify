321e61bc36e8e688da02fb7ced38ad8d1da18025
Martin Kamír <martin.k.kamir@gmail.com>
2023-12-02 19:00:54 +0100

    delete fetchFn in createApi

--- src/features/api/api.js
+++ src/features/api/api.js
@@ -4,11 +4,6 @@ export const api = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
         baseUrl: "https://forkify-api.herokuapp.com/api/v2/recipes",
-        // fetchFn: async (...args) => {
-        //     console.log(...args);
-        //     await new Promise(resolve => setTimeout(resolve, 11_000));
-        //     return fetch(...args);
-        // },
     }),
     tagTypes: ["Recipe"],
     endpoints: () => ({}),
